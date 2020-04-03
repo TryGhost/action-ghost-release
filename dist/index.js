7139d165e931ddf12454d0138f13de7f235d2e5b
Daniel Lockyer <hi@daniellockyer.com>
2020-04-03 14:37:07 +0100

    Formatted code according to the linter
    no issue

--- dist/index.js
+++ dist/index.js
@@ -2866,7 +2866,6 @@ const basePath = process.env.GITHUB_WORKSPACE || process.cwd();
 
 const ghostPackageInfo = JSON.parse(fs.readFileSync(path.join(basePath, 'package.json')));
 const zipName = `Ghost-${ghostPackageInfo.version}.zip`;
-let gistUrl, githubUploadURL;
 
 function getPreviousVersion(tags) {
     const sameMajorReleaseTags = [], otherReleaseTags = [];
@@ -2888,79 +2887,78 @@ function getPreviousVersion(tags) {
     return (sameMajorReleaseTags.length !== 0) ? sameMajorReleaseTags[0] : otherReleaseTags[0];
 }
 
-releaseUtils.releases.get({
-    userAgent: 'ghost-release',
-    uri: `https://api.github.com/repos/${ORGNAME}/Ghost/releases`
-})
-.then(getPreviousVersion)
-.then((previousVersion) => {
-    const changelog = new releaseUtils.Changelog({
+releaseUtils.releases
+    .get({
+        userAgent: 'ghost-release',
+        uri: `https://api.github.com/repos/${ORGNAME}/Ghost/releases`
+    })
+    .then(getPreviousVersion)
+    .then((previousVersion) => {
+        const changelog = new releaseUtils.Changelog({
+            changelogPath: path.join(basePath, 'changelog.md'),
+            folder: process.cwd()
+        });
+
+        changelog
+            .write({
+                githubRepoPath: `https://github.com/${ORGNAME}/Ghost`,
+                lastVersion: previousVersion
+            })
+            .write({
+                githubRepoPath: `https://github.com/${ORGNAME}/Ghost-Admin`,
+                lastVersion: previousVersion,
+                append: true,
+                folder: path.join(basePath, 'core', 'client')
+            })
+            .sort()
+            .clean();
+
+        console.log('changelog.md generated');
+        return Promise.resolve();
+    })
+    .then(() => releaseUtils.gist.create({
+        userAgent: 'ghost-release',
+        gistName: 'changelog-' + ghostPackageInfo.version + '.md',
+        gistDescription: 'Changelog ' + ghostPackageInfo.version,
         changelogPath: path.join(basePath, 'changelog.md'),
-        folder: process.cwd()
+        github: {
+            token: process.env.RELEASE_TOKEN
+        },
+        isPublic: true
+    }))
+    .then((response) => {
+        console.log(`Gist generated: ${response.gistUrl}`);
+        return Promise.resolve(response);
+    })
+    .then(response => releaseUtils.releases.create({
+        draft: true,
+        preRelease: false,
+        tagName: ghostPackageInfo.version,
+        releaseName: ghostPackageInfo.version + '+banana',
+        userAgent: 'ghost-release',
+        uri: `https://api.github.com/repos/${ORGNAME}/Ghost/releases`,
+        github: {
+            token: process.env.RELEASE_TOKEN
+        },
+        changelogPath: [{changelogPath: path.join(basePath, 'changelog.md')}],
+        gistUrl: response.gistUrl
+    }))
+    .then((response) => {
+        console.log(`Release draft generated: ${response.releaseUrl}`);
+        return Promise.resolve(response);
+    })
+    .then(response => releaseUtils.releases.uploadZip({
+        github: {
+            token: process.env.RELEASE_TOKEN
+        },
+        zipPath: path.join(basePath, '.dist', 'release', zipName),
+        uri: `${response.uploadUrl.substring(0, response.uploadUrl.indexOf('{'))}?name=${zipName}`,
+        userAgent: 'ghost-release'
+    }))
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
     });
-
-    changelog
-        .write({
-            githubRepoPath: `https://github.com/${ORGNAME}/Ghost`,
-            lastVersion: previousVersion
-        })
-        .write({
-            githubRepoPath: `https://github.com/${ORGNAME}/Ghost-Admin`,
-            lastVersion: previousVersion,
-            append: true,
-            folder: path.join(basePath, 'core', 'client')
-        })
-        .sort()
-        .clean();
-
-    console.log('changelog.md generated');
-    return Promise.resolve();
-})
-.then(() => releaseUtils.gist.create({
-    userAgent: 'ghost-release',
-    gistName: 'changelog-' + ghostPackageInfo.version + '.md',
-    gistDescription: 'Changelog ' + ghostPackageInfo.version,
-    changelogPath: path.join(basePath, 'changelog.md'),
-    github: {
-        token: process.env.RELEASE_TOKEN
-    },
-    isPublic: true
-}))
-.then((response) => {
-    gistUrl = response.gistUrl;
-    console.log(`Gist generated: ${gistUrl}`);
-    return Promise.resolve();
-})
-.then(() => releaseUtils.releases.create({
-    draft: true,
-    preRelease: false,
-    tagName: ghostPackageInfo.version,
-    releaseName: ghostPackageInfo.version + '+banana',
-    userAgent: 'ghost-release',
-    uri: `https://api.github.com/repos/${ORGNAME}/Ghost/releases`,
-    github: {
-        token: process.env.RELEASE_TOKEN
-    },
-    changelogPath: [{changelogPath: path.join(basePath, 'changelog.md')}],
-    gistUrl: gistUrl
-}))
-.then((response) => {
-    githubUploadURL = response.uploadUrl;
-    console.log(`Release draft generated: ${response.releaseUrl}`);
-    return Promise.resolve();
-})
-.then(() => releaseUtils.releases.uploadZip({
-    github: {
-        token: process.env.RELEASE_TOKEN
-    },
-    zipPath: path.join(basePath, '.dist', 'release', zipName),
-    uri: `${githubUploadURL.substring(0, githubUploadURL.indexOf('{'))}?name=${zipName}`,
-    userAgent: 'ghost-release'
-}))
-.catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
 
 
 /***/ }),
